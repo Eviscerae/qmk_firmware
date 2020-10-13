8dde379e4e8d5e9c726be47eeef4eb29d9260e2e
Allen Reese <eviscerae@gmail.com>
2020-10-12 21:01:32 -0400

    adding num and scroll lock and changing numkeys to actual numpad key codes.

--- keyboards/preonic/keymaps/eviscerae/keymap.c
+++ keyboards/preonic/keymaps/eviscerae/keymap.c
@@ -131,7 +131,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | TRNS |  F7  |  F8  |  F9  | F10  | F11  | F12  |   4  |   5  |   6  |   +  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Caps |Ctrl+A|Ctrl+S|Ctrl+D|Ctrl+F| TRNS | TRNS |   1  |   2  |   3  |   -  |  "   |
+ * | Caps |Ctrl+A|Ctrl+S|Ctrl+D|Ctrl+F|  KC  |  KC  |   1  |   2  |   3  |   -  |  "   |
+ * |      |      |      |      |      | _NLCK| _SLCK|      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|Ctrl+Z|Ctrl+X|Ctrl+C|Ctrl+V| TRNS | TRNS |   0  |   0  |   .  |SHFTHM|SHFTEN|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -139,10 +140,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 	[_LOWER] = LAYOUT_preonic_1x2uC(
-        KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_P7,    KC_P8,    KC_P9,    KC_PEQL,  KC_DEL,
-        KC_TRNS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,  KC_PAST,
-        KC_CAPS,  CTL_A,    CTL_S,    CTL_D,    CTL_F,    KC_TRNS,  KC_TRNS,  KC_P1,    KC_P2,    KC_P3,    KC_PMNS,  KC_PSLS,
-        KC_TRNS,  CTL_Z,    CTL_X,    CTL_C,    CTL_V,    KC_TRNS,  KC_TRNS,  KC_P0,    KC_P0,    KC_PDOT,  SHFT_HME, SHFT_END,
+        KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_PEQL,  KC_DEL,
+        KC_TRNS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_PPLS,  KC_PAST,
+        KC_CAPS,  CTL_A,    CTL_S,    CTL_D,    CTL_F,    KC_NLCK,  KC_SLCK,  KC_KP_1,  KC_KP_2,  KC_KP_3,  KC_PMNS,  KC_PSLS,
+        KC_TRNS,  CTL_Z,    CTL_X,    CTL_C,    CTL_V,    KC_TRNS,  KC_TRNS,  KC_KP_0,  KC_KP_0,  KC_PDOT,  SHFT_HME, SHFT_END,
         COMBOS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,       WIN_LOCK,      ADJUST,   KC_HOME,  KC_PGUP,  KC_PGDN,  KC_END),
 
 /* Raise
